047c5c6578942c7ed1cda4276099cc812d7ad062
sairajgad06 <sairajgad06@gmail.com>
2023-09-02 21:36:57 +0530

    Create 209. Minimum Size Subarray Sum.java

---
+++ top150interview/209. Minimum Size Subarray Sum.java
@@ -0,0 +1,53 @@
+// Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+// subarray
+//  whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+ 
+
+// Example 1:
+
+// Input: target = 7, nums = [2,3,1,2,4,3]
+// Output: 2
+// Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+// Example 2:
+
+// Input: target = 4, nums = [1,4,4]
+// Output: 1
+// Example 3:
+
+// Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+// Output: 0
+ 
+
+// Constraints:
+
+// 1 <= target <= 109
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 104
+ 
+
+// // Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
+
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        int left =0;
+        int right =0;
+        int min = Integer.MAX_VALUE;
+        while(right<n){
+            sum+=nums[right];
+            if(sum>=target){
+                while(sum>=target){
+                    sum-=nums[left];
+                    left++;
+                }
+                min = Math.min(min, right-left+2);
+            }
+            right++;
+        }
+        
+        return min==Integer.MAX_VALUE?0:min;
+    }
+}
