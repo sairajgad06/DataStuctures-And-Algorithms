5cfc116ce91c9c81bb006525a2069218aaf8de94
sairajgad06 <sairajgad06@gmail.com>
2023-09-05 22:02:19 +0530

    Create 9. Palindrome Number.java

---
+++ top150interview/9. Palindrome Number.java
@@ -0,0 +1,47 @@
+// Given an integer x, return true if x is a 
+// palindrome
+// , and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+ 
+
+// Follow up: Could you solve it without converting the integer to a string?
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        int rev =0;
+        while(x>0){
+            int digit = x%10;
+            rev =(rev*10)+digit;
+            x=x/10;
+        }
+
+        if(rev==temp){
+            return true;
+        }
+
+        return false;
+    }
+}
